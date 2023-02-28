3c51854dec4536ce798ad6604b8b74e21027785f
Lucio Correa <labcorrea@gmail.com>
2023-02-28 09:33:53 -0300

    [ADD] pip installer

---
+++ GeoMeshInstallPIP.sh
@@ -0,0 +1,17 @@
+python -m pip install utm
+python -m pip install pyproj
+python -m pip install pyvista
+python -m pip install pygmtsar
+python -m pip install matplotlib
+python -m pip install holoviews
+python -m pip install dask
+python -m pip install xarray
+python -m pip install numpy
+python -m pip install pandas
+python -m pip install hvplot
+python -m pip install wheel
+python -m pip install setuptools
+python -m pip install twine
+python -m pip uninstall geohexamesher
+python setup.py bdist_wheel
+pip install dist/geohexamesher-1-py3-none-any.whl
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='geohexamesher',
+    packages=['src'],
+    version='1',
+    description='Hexaedrical mesher for SEM3D and SEM applications',
+    author='SEM',
+    license='GNU',
+)
