f5a45ff64fb3d790baa699125d5fd463a3ad155a
Lucio Correa <labcorrea@gmail.com>
2023-03-06 19:26:18 -0300

    [ADD] get GEBCO database in get DEM Model

--- src/DEMClass.py
+++ src/DEMClass.py
@@ -15,10 +15,14 @@ class DEMClass:
         #  @var STLMesh Attribute to store the Lat and Long
         self.STLMesh = np.array([])
 
-    def GetMaterialProperty(self,Inp):
+    def GetMaterialProperty(self,Inp,OutType=None):
         ## Get the material model
         # @param Inp the input (InputClass) object
-        
+        # #param OutType 0 if only save h5, 1 only return, 2 save and return
+
+        if OutType is None:
+            Outtype = 2
+
         InpDir = Inp.WorkDir
         MatType = Inp.PropertyModel
         lim = Inp.Lim
@@ -37,24 +41,102 @@ class DEMClass:
             import rockhound as rh
             prem = rh.fetch_prem()
             # filter the depth
-            print(depth)
             df = prem.loc[prem['depth'] <= depth ]
-            print(df)
-            #save the prem model in the folder
-
+            l = np.zeros((df.depth.size,4))
+            for i in range(0,df.depth.size):
+                l[i,0] = lim[0]
+                l[i,1] = lim[1]
+                l[i,2] = lim[2]
+                l[i,3] = lim[3]
+            #print(l.shape)
+            df['LatMin'] = l[:,0]
+            df['LatMax'] = l[:,1]
+            df['LonMin'] = l[:,2]
+            df['LonMax'] = l[:,3]
+            #print(df)
+            if OutType == 0:
+                # Save the DataFrame in HDF5 format
+                h5filename = path+os.path.sep+"Material.h5"
+                df.to_hdf(h5filename, key='material', mode='w')
+            elif OutType == 1:
+                return df
+            elif OutType == 2:
+                h5filename = path+os.path.sep+"Material.h5"
+                print(h5filename)
+                df.to_hdf(h5filename, key='material', mode= 'w')
+                return df
         else:
-            print("This material model is not supported by GeoHexMesher")    
+            print("This material model is not supported by GeoHexMesher")
+            sys.exit(1)
 
-
-        #download the data in the path
-
-    def GetDEMModel(self,lim):
+    def GetDEMModel(self,Inp,OutType):
         ## Get the DEM model
+        # @param Inp the input (InputClass) object
+        # #param OutType 0 if only save h5, 1 only return, 2 save and return
+
+        if OutType is None:
+            Outtype = 2
+        
+        InpDir = Inp.WorkDir
+        DEMModel = Inp.DEMModel
+        lim = Inp.Lim
+
         path = InpDir+os.path.sep+"DEM"
+        
+        if DEMModel == "GEBCO":
+            path = path+os.path.sep+"GEBCO"
+            filepath = path+os.path.sep+"GEBCO_2022_sub_ice_topo.nc"
 
+            if not os.path.isfile(filepath):
+                import requests, zipfile, io
+                print("This operation can take several hours")
+                zipurl = 'https://www.bodc.ac.uk/data/open_download/gebco/gebco_2022_sub_ice_topo/zip/'
+                #r = requests.get(zipurl)
+                #z = zipfile.ZipFile(io.BytesIO(r.content))
+                #z.extractall(path)
+            else:
+                #do nothing
+                print("The files has been found in your computer")
 
+            import xarray as xr
+            ds = xr.open_dataset(filepath)
 
-        #mesma coisa do de cima
+            # Set the limits of the plot
+            minlon, maxlon, minlat, maxlat = lim[2], lim[3], lim[0], lim[1]
+            subset = ds.sel(lon=slice(minlon,maxlon))
+            subset = subset.sel(lat=slice(minlat, maxlat))
+
+            # Extract the relevant data variables
+            lon = subset.lon.values
+            lat = subset.lat.values
+            depth = subset.elevation.values
+
+        elif DEMModel == "ETOPO1":
+
+        elif DEMModel == "ETOPO2":
+
+        elif DEMModel == "GMSTAR":
+
+        elif DEMModel == "AW3D":
+                #https://www.aw3d.jp/en/
+        else:
+            print("This DEM model is not supported by GeoHexMesher")
+            sys.exit(2)
+        
+       import pandas as pd
+       out = pd.DataFrame({'lat':lat,'lon':lon,'z':depth})
+       #print(df)
+       if OutType == 0:
+            # Save the DataFrame in HDF5 format
+            h5filename = path+os.path.sep+"DEM.h5"
+            out.to_hdf(h5filename, key='DEM', mode='w')
+       elif OutType == 1:
+            return out
+       elif OutType == 2:
+            h5filename = path+os.path.sep+"DEM.h5"
+            print(h5filename)
+            out.to_hdf(h5filename, key='DEM', mode= 'w')
+            return out
 
     def GetSurfaces(self,lim):
         ## Manage the methods to return the final input to polycube
--- src/testCode.py
+++ src/testCode.py
@@ -1,30 +1,28 @@
-from MeshClass import MeshClass
-from ElementClass import ElementClass
-from NodeClass import NodeClass
-from GMTSARClass import GMTSARClass
+#from MeshClass import MeshClass
+#from ElementClass import ElementClass
+#from NodeClass import NodeClass
+from DEMClass import DEMClass
 from InputClass import InputClass
-from PolyCubeClass import PolyCubeClass
+#from PolyCubeClass import PolyCubeClass
 
-M = MeshClass()
-E = ElementClass()
-N = NodeClass()
-G = GMTSARClass()
+#M = MeshClass()
+#E = ElementClass()
+#N = NodeClass()
+D = DEMClass()
 I = InputClass()
-P = PolyCubeClass()
+#P = PolyCubeClass()
 
 
 
-print(M)
-print(E)
-print(N)
-print(G)
-print(I)
-print(P)
+#print(M)
+#print(E)
+#print(N)
+#print(G)
+#print(I)
+#print(P)
 
 
 I.ReadConfigFile('config.txt')
 
-print('Lim:', I.Lim)
-print('Elz:', I.Elz)
-print('Elxy:', I.Elxy)
-print('ZMax:', I.ZMax)
+print(I)
+D.GetMaterialProperty(I,2)
